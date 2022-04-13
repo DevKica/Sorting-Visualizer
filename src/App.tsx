43d223064ae2f00bc9fff613a4dcf25d9c1644a8
DevKica <devKica777@gmail.com>
2022-04-13 11:08:22 +0200

    project init

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="min-h-screen w-screen overflow-hidden bg-neutral-700 break-words">
+      <p>helo</p>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+//main
+import App from "./App";
+import React from "react";
+//styles
+import "./styles/index.css";
+//react
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
